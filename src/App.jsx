1037d7d1e6820f7aa9807848aaa1517880457e57
Samuel <samuelpedro0001@gmail.com>
2023-07-21 16:21:40 -0300

    Entrega concluida

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-function App() {
+import HeaderApp from "./Componets/Header";
+import HeaderContentApp from "./Componets/Header/HeaderContent";
+import MainContentApp from "./Componets/Header/MainContent";
+import SectionAboutMe from "./Componets/Header/SectionDesc";
+import RenderTechnologies from "./Componets/Header/TechnologiesSection";
+import { technologies } from "../src/data/technologies.js";
+import "./styles/index.css";
+import RenderProjects from "./Componets/Header/SectionProjects";
+import { projects } from "../src/data/projects.js";
+import Contact from "./Componets/Header/Contact";
+import "./styles/globalstyles.css";
 
+function App() {
   return (
     <>
-      
+      <HeaderApp>
+        <HeaderContentApp />
+      </HeaderApp>
+      <MainContentApp />
+      <SectionAboutMe />
+      <RenderTechnologies cardsList={technologies} />
+      <RenderProjects listProjects={projects} />
+      <Contact />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
